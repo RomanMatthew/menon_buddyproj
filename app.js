b2d0fbf96b3906b730ecddac0c755cba44d2c27d
Davis Tran <davistran0119@gmail.com>
2021-03-16 17:18:31 -0400

    calls to api and updated front-end with bootstrap

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   var temp = document.querySelector('.temp');
   var desc = document.querySelector('.desc');
   var clouds = document.querySelector('.clouds');
-  var button = document.querySelector('.submit');
+  var button = document.querySelector('.btn-outline-primary');
 
   button.addEventListener('click', function(name) {
       fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=279e2439d044335148c1233102102740')
@@ -12,11 +12,12 @@
               var tempValue = data['main']['temp'];
               var nameValue = data['name'];
               var descValue = data['weather'][0]['description'];
-
+              var cloudValue = data['clouds']['all']
               main.innerHTML = nameValue;
               // temp must be converted from kelvin to fahrenheit
               temp.innerHTML = "Temperature: " + (Math.round(((tempValue - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
               desc.innerHTML = "Outlook: " + descValue;
+              clouds.innerHTML = "Cloudiness: " + cloudValue + "%";
               input.value = "";
           })
           .catch(err => alert("Wrong city name!"));
