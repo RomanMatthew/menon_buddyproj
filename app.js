560d463803e541621f1b723dcdff274aa09249af
Davis Tran <davistran0119@gmail.com>
2021-03-17 18:12:21 -0400

    5 day data utilized, populated and filtered properly on data display

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 // query
 var input = document.querySelector('.input_text');
+var query = document.querySelector('.btn-outline-primary'); // button press
 // name of the query
 var main = document.querySelector('#name');
+// CURR DAY
 var temp = document.querySelector('.temp'); // temp
 var date = document.querySelector('.date'); // curr date
 var desc = document.querySelector('.desc'); // description
 var clouds = document.querySelector('.clouds'); // cloudiness
-var query = document.querySelector('.btn-outline-primary'); // button press
+// IN 24 HRS
+var temp1 = document.querySelector('.temp1'); // temp
+var date1 = document.querySelector('.date1'); // curr date
+var desc1 = document.querySelector('.desc1'); // description
+var clouds1 = document.querySelector('.clouds1'); // cloudiness
+// IN 48 HRS
+var temp2 = document.querySelector('.temp2'); // temp
+var date2 = document.querySelector('.date2'); // curr date
+var desc2 = document.querySelector('.desc2'); // description
+var clouds2 = document.querySelector('.clouds2'); // cloudiness
+// IN 72 HRS
+var temp3 = document.querySelector('.temp3'); // temp
+var date3 = document.querySelector('.date3'); // curr date
+var desc3 = document.querySelector('.desc3'); // description
+var clouds3 = document.querySelector('.clouds3'); // cloudiness
+// IN 96 HRS
+var temp4 = document.querySelector('.temp4'); // temp
+var date4 = document.querySelector('.date4'); // curr date
+var desc4 = document.querySelector('.desc4'); // description
+var clouds4 = document.querySelector('.clouds4'); // cloudiness
 
 var tempBtn = document.getElementById("tempBtn")
 var outlookBtn = document.getElementById("outlookBtn")
@@ -32,18 +53,48 @@ function getData() {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input.value + '&appid=279e2439d044335148c1233102102740')
         .then(response => response.json())
         .then(data => {
-            //   var dateValue = data['timezone']
             var tempValue = data['list'][0]['main']['temp'];
-            var nameValue = data['city']['name'];
+            var cityValue = data['city']['name'];
+            var countryValue = data['city']['country'];
             var descValue = data['list'][0]['weather'][0]['description'];
             var cloudValue = data['list'][0]['clouds']['all']
-            main.innerHTML = nameValue;
+            main.innerHTML = cityValue + ', ' + countryValue;
             // temp must be converted from kelvin to fahrenheit
             temp.innerHTML = (Math.round(((tempValue - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
             desc.innerHTML = descValue;
-            //   date.innerHTML = dateValue;
             clouds.innerHTML = cloudValue + "%";
-            //   input.value = "";
+            // NEXT DAY
+            var tempValue1 = data['list'][8]['main']['temp'];
+            var descValue1 = data['list'][8]['weather'][0]['description'];
+            var cloudValue1 = data['list'][8]['clouds']['all']
+                // temp must be converted from kelvin to fahrenheit
+            temp1.innerHTML = (Math.round(((tempValue1 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            desc1.innerHTML = descValue1;
+            clouds1.innerHTML = cloudValue1 + "%";
+            // NEXT DAY
+            var tempValue2 = data['list'][16]['main']['temp'];
+            var descValue2 = data['list'][16]['weather'][0]['description'];
+            var cloudValue2 = data['list'][16]['clouds']['all']
+                // temp must be converted from kelvin to fahrenheit
+            temp2.innerHTML = (Math.round(((tempValue2 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            desc2.innerHTML = descValue2;
+            clouds2.innerHTML = cloudValue2 + "%";
+            // NEXT DAY
+            var tempValue3 = data['list'][24]['main']['temp'];
+            var descValue3 = data['list'][24]['weather'][0]['description'];
+            var cloudValue3 = data['list'][24]['clouds']['all']
+                // temp must be converted from kelvin to fahrenheit
+            temp3.innerHTML = (Math.round(((tempValue3 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            desc3.innerHTML = descValue3;
+            clouds3.innerHTML = cloudValue3 + "%";
+            // NEXT DAY
+            var tempValue4 = data['list'][32]['main']['temp'];
+            var descValue4 = data['list'][32]['weather'][0]['description'];
+            var cloudValue4 = data['list'][32]['clouds']['all']
+                // temp must be converted from kelvin to fahrenheit
+            temp4.innerHTML = (Math.round(((tempValue4 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            desc4.innerHTML = descValue4;
+            clouds4.innerHTML = cloudValue4 + "%";
         })
         .catch(err => alert("Wrong city name!"));
 }
@@ -52,9 +103,17 @@ function getTemps() {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input.value + '&appid=279e2439d044335148c1233102102740')
         .then(response => response.json())
         .then(data => {
-            var tempValue = data['list'][0]['main']['temp'];
             // temp must be converted from kelvin to fahrenheit
+            var tempValue = data['list'][0]['main']['temp'];
             temp.innerHTML = (Math.round(((tempValue - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            var tempValue1 = data['list'][8]['main']['temp'];
+            temp1.innerHTML = (Math.round(((tempValue1 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            var tempValue2 = data['list'][16]['main']['temp'];
+            temp2.innerHTML = (Math.round(((tempValue2 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            var tempValue3 = data['list'][24]['main']['temp'];
+            temp3.innerHTML = (Math.round(((tempValue3 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
+            var tempValue4 = data['list'][32]['main']['temp'];
+            temp4.innerHTML = (Math.round(((tempValue4 - 273.15) * (9 / 5) + 32) * 10) / 10) + " deg. F";
         })
         .catch(err => alert("Wrong city name!"));
 }
@@ -65,6 +124,14 @@ function getOutlook() {
         .then(data => {
             var descValue = data['list'][0]['weather'][0]['description'];
             desc.innerHTML = descValue;
+            var descValue1 = data['list'][8]['weather'][0]['description'];
+            desc1.innerHTML = descValue1;
+            var descValue2 = data['list'][16]['weather'][0]['description'];
+            desc2.innerHTML = descValue2;
+            var descValue3 = data['list'][24]['weather'][0]['description'];
+            desc3.innerHTML = descValue3;
+            var descValue4 = data['list'][32]['weather'][0]['description'];
+            desc4.innerHTML = descValue4;
         })
         .catch(err => alert("Wrong city name!"));
 }
@@ -75,6 +142,18 @@ function getCloudiness() {
         .then(data => {
             var cloudValue = data['list'][0]['clouds']['all']
             clouds.innerHTML = cloudValue + "%";
+            // NEXT DAY
+            var cloudValue1 = data['list'][8]['clouds']['all']
+            clouds1.innerHTML = cloudValue1 + "%";
+            // NEXT DAY
+            var cloudValue2 = data['list'][16]['clouds']['all']
+            clouds2.innerHTML = cloudValue2 + "%";
+            // NEXT DAY
+            var cloudValue3 = data['list'][24]['clouds']['all']
+            clouds3.innerHTML = cloudValue3 + "%";
+            // NEXT DAY
+            var cloudValue4 = data['list'][32]['clouds']['all']
+            clouds4.innerHTML = cloudValue4 + "%";
         })
         .catch(err => alert("Wrong city name!"));
 }
@@ -83,4 +162,16 @@ function clear() {
     temp.innerHTML = "";
     desc.innerHTML = "";
     clouds.innerHTML = "";
+    temp1.innerHTML = "";
+    desc1.innerHTML = "";
+    clouds1.innerHTML = "";
+    temp2.innerHTML = "";
+    desc2.innerHTML = "";
+    clouds2.innerHTML = "";
+    temp3.innerHTML = "";
+    desc3.innerHTML = "";
+    clouds3.innerHTML = "";
+    temp4.innerHTML = "";
+    desc4.innerHTML = "";
+    clouds4.innerHTML = "";
 }
